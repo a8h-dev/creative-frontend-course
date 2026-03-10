a217ca7f089c9274dc5c547821a3aef28aff09c5
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-03-10 13:30:01 +0530

    landing page project started and GSAP setup done

---
+++ jsm_gsap_landing/src/App.jsx
@@ -0,0 +1,14 @@
+import { ScrollTrigger, SplitText } from "gsap/all"
+import gsap from "gsap/gsap-core.js";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div>
+      App
+    </div>
+  );
+}
+
+export default App;
