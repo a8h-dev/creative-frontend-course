179650ca8ba6a152de5a1091cae106d6cd49c8df
Abhishek Mahto <dev.abhishekmahto@gmail.com>
2026-03-10 13:43:19 +0530

    css file added to

--- jsm_gsap_landing/src/App.jsx
+++ jsm_gsap_landing/src/App.jsx
@@ -5,8 +5,8 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Hello World!</h1>
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-7xl">Hello World!</h1>
     </div>
   );
 }
